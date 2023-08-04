ea14109756dad8b6dccab414ab6950f342775f52
Mbuyiselo91 <mbuyisolo@gmail.com>
2023-08-04 14:33:23 +0200

    added footer

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Footer from "./pages/Footer.jsx"
+import Home from "./pages/Home.jsx"
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/footer",
+    element: <Footer/>,
+  },
+  {
+    path: "/home",
+    element: <Home/>,
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <RouterProvider router={router} />
     <App />
   </React.StrictMode>,
 )
