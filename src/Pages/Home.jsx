bb56c69b4aa980faf04848e937e4e6364bd52e8d
Melusi5 <melusimngomezulu5@gmail.com>
2023-08-04 15:34:06 +0200

    change picture

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export default function Home() {
+export default function picture() {
   return (
     <div>
-      Home
+      picture 
     </div>
   )
 }
+export default 
